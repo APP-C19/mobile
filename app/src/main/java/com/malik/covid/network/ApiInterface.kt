ae0c325ff85d61c19fc748cd4b54a738edc0d6c9
Malik Dawar <malik.dawar@couriootechnologies.com>
2020-04-30 16:56:47 +0500

    Removed unnecessary model class User.kt

--- app/src/main/java/com/malik/covid/network/ApiInterface.kt
+++ app/src/main/java/com/malik/covid/network/ApiInterface.kt
@@ -1,11 +1,6 @@
 package com.malik.covid.network
 
-import com.malik.covid.models.User
-import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.POST
 
 interface ApiInterface {
-    @POST("")
-    fun uploadData(@Body body: HashMap<String, Any>): Call<User>
+
 }
